2556727104aff7513b8ebaf54b2427ee98cc09e0
gitgitWi <gitgitWi@gmail.com>
2025-08-14 02:06:41 +0900

    feat(macos): update deps to latest

--- apps/macos/vite.config.ts
+++ apps/macos/vite.config.ts
@@ -1,5 +1,5 @@
 import path from 'node:path';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import electron from 'vite-plugin-electron/simple';
 
