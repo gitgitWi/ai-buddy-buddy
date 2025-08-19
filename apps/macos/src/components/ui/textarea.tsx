be4e1174efe3dea539327c0f877dc1de29f248e5
gitgitWi <gitgitWi@gmail.com>
2025-08-20 03:24:26 +0900

    feat(macos/ui): add textarea-autosize

--- apps/macos/src/components/ui/textarea.tsx
+++ apps/macos/src/components/ui/textarea.tsx
@@ -1,9 +1,15 @@
 import type { ComponentProps } from 'react';
+import TextareaAutosize, {
+  type TextareaAutosizeProps,
+} from 'react-textarea-autosize';
 import { cn } from '~/lib/utils';
 
-function Textarea({ className, ...props }: ComponentProps<'textarea'>) {
+function Textarea({
+  className,
+  ...props
+}: Omit<ComponentProps<'textarea'>, 'style'> & TextareaAutosizeProps) {
   return (
-    <textarea
+    <TextareaAutosize
       className={cn(
         'field-sizing-content flex min-h-16 w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-xs outline-none transition-[color,box-shadow] placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:aria-invalid:ring-destructive/40',
         className
