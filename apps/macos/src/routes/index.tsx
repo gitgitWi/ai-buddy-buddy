e0d70aeea4363c4d3ef5929181a004b6f29bf59e
gitgitWi <gitgitWi@gmail.com>
2025-08-20 03:34:30 +0900

    feat(macos/routes): add title to main page

--- apps/macos/src/routes/index.tsx
+++ apps/macos/src/routes/index.tsx
@@ -1,9 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { Text } from '~/components/ui/text';
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>;
+  return (
+    <div className="relative flex h-full w-full flex-col items-center justify-center gap-y-2 pb-[20%]">
+      <Text.H1 className="select-none text-neutral-950 dark:text-neutral-50">
+        AI BuddyBuddy
+      </Text.H1>
+    </div>
+  );
 }
