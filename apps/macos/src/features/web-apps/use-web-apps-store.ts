be2a5752f51cb6d897636f9f14cb046f13e97577
gitgitWi <gitgitWi@gmail.com>
2025-08-21 23:46:48 +0900

    feat(macos/web-apps): add zustand states/actions for selected

--- apps/macos/src/features/web-apps/use-web-apps-store.ts
+++ apps/macos/src/features/web-apps/use-web-apps-store.ts
@@ -2,14 +2,46 @@ import { create } from 'zustand';
 import { OPTIONS, Tags, type WebAppOption } from './constants';
 
 type WebAppStore = {
-  selected: Set<WebAppOption>;
-  setSelected: (selected: Set<WebAppOption>) => void;
+  selected: WebAppOption[];
+  unselected: WebAppOption[];
+  toggleSelected: (option: WebAppOption) => void;
+  changeOrder: (option: WebAppOption, order: number) => void;
 };
 
 /** @todo store in main process SQLite */
 export const useWebAppsStore = create<WebAppStore>((set) => ({
-  selected: new Set(
-    OPTIONS.filter((option) => (option.tag as Set<Tags>).has(Tags.Default))
+  selected: OPTIONS.filter((option) =>
+    (option.tag as Set<Tags>).has(Tags.Default)
   ),
-  setSelected: (selected) => set({ selected }),
+
+  unselected: OPTIONS.filter(
+    (option) => !(option.tag as Set<Tags>).has(Tags.Default)
+  ),
+
+  toggleSelected: (option) =>
+    set((prev) => {
+      const hasOption = prev.selected.find((val) => val.url === option.url);
+      if (hasOption) {
+        return {
+          selected: prev.selected.filter((val) => val.url !== option.url),
+          unselected: [option].concat(prev.unselected),
+        };
+      }
+
+      return {
+        selected: prev.selected.concat(option),
+        unselected: prev.unselected.filter((val) => val.url !== option.url),
+      };
+    }),
+
+  changeOrder: (option, order) =>
+    set((prev) => {
+      const filtered = prev.selected.filter((val) => val.url !== option.url);
+
+      return {
+        selected: filtered
+          .slice(0, order)
+          .concat(option, filtered.slice(order)),
+      };
+    }),
 }));
